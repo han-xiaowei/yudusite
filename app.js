672416bf4e4bcc572c460dec74af67a9858c8749
han-xiaowei <404001951@qq.com>
2017-05-21 11:40:51 +0800

    test

--- app.js
+++ app.js
@@ -14,8 +14,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.locals.title = '誉度装饰';
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use('/file', express.static('public'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
+});
+
+router.get('/hxw', function(req, res, next) {
+    res.render('users');
 });
 
 module.exports = router;
